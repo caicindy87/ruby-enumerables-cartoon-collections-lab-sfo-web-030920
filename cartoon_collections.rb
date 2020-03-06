bc64564c626b9ef7be721cc3521ae54f47f43789
Cindy Cai <caicindy87@gmail.com>
2020-03-06 04:16:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,9 @@
 require 'pry'
 
 def roll_call_dwarves(array)
-  
+  array.each_with_index do |item, index|
+    puts "#{index + 1}. #{item}"
+  end
 end
 
 def summon_captain_planet# code an argument here
